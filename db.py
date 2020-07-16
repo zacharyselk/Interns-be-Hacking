e0c76b937553f0152a153ebbf0540a78a8bc9397
zacharyselk <zrselk@gmail.com>
2020-07-16 08:16:43 -0600

    Added simple database api

---
+++ db.py
@@ -0,0 +1,94 @@
+from cloudant.client import Cloudant
+import mimetypes
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import io
+
+
+API_KEY = {
+  "apikey": "ao7tLVel9Ms4WaFe0gXrGXYlvPZ4o3oqm9C8E0O3r3x3",
+  "host": "3f867ad1-b1df-471c-935a-5bafbeb180c4-bluemix.cloudantnosqldb.appdomain.cloud",
+  "iam_apikey_description": "Auto-generated for key b213f645-1734-42d5-9988-55a7099110dc",
+  "iam_apikey_name": "MVPkey",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/4bde1a5977a349e4b2943360ec53eae4::serviceid:ServiceId-357ed28e-4a5c-4ed2-bd81-c5ae900e74bc",
+  "url": "https://3f867ad1-b1df-471c-935a-5bafbeb180c4-bluemix.cloudantnosqldb.appdomain.cloud",
+  "username": "3f867ad1-b1df-471c-935a-5bafbeb180c4-bluemix"
+}
+
+
+class Assignment:
+    def __init__(self, _id, title, text, due_date, completed=False):
+        self._id = _id
+        self.title = title
+        self.text = text
+        self.due_date = due_date
+        self.completed = completed
+
+    def get_data(self):
+        return {
+            "_id": self._id,
+            "Title": self.title,
+            "Text": self.text,
+            "Due Date": self.due_date,
+            "Completed": self.completed
+        }
+
+
+class Database:
+    def __init__(self, username, apikey):
+        self.client = Cloudant.iam(username, apikey, connect=True)
+        self.database = self.client['ibh']
+
+
+    def disconnect(self):
+        self.client.disconnect()
+
+
+    def create_assignment(self, assignment):
+        return self.database.create_document(assignment.get_data())
+
+
+    def list_assignments(self):
+        db_assignments = self.database.all_docs()['rows']
+        assignment_ids = list(map(lambda x: x['id'], db_assignments))
+        return assignment_ids
+
+
+    def get_assignments(self):
+        assignments = []
+        for assignment in self.list_assignments():
+            asn = self.database[assignment]
+            print(asn)
+            assignments.append(Assignment(asn['_id'], asn['Title'], asn['Text'],
+                                          asn['Due Date'], asn['Completed']))
+        print(assignments)
+        return assignments
+
+
+    def add_attachment(self, document, name):
+        with open(name, "rb") as f:
+            content_type = mimetypes.MimeTypes().guess_type('test-img.jpg')[0]
+            main_type, sub_type = content_type.split('/', 1)
+            msg = f.read()
+            document.put_attachment(name, content_type, msg)
+
+
+    def get_data(self, assignment):
+        for assgn in self.database.all_docs()['rows']:
+            if assgn['id'] == assignment:
+                return assign
+        return None
+
+
+db = Database(API_KEY["username"], API_KEY["apikey"])
+
+asn1 = Assignment("Assignment001", "History", "Learn about the civil war", "15/07/20")
+db.create_assignment(asn1)
+db.get_assignments()
+
+db.disconnect()
